f74f23aca21a453f7d044d9e4b987fa376bbb1e3
John Gonsalves <thomasgonsalves12345@gmail.com>
2025-08-01 13:32:26 -0500

    working on new portfolio project

---
+++ src/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <header className="fixed top-0 left-0 right-0 z-50 bg-black/90">
+      <div className="flex justify-between items-center py-5 mx-auto c-space">
+        <a
+          href="/"
+          className="text-neutral-400 font-bold text-xl hover:text-white transition-colors"
+        ></a>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
