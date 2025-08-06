c2dfae55f9e0ea0f620d912e5a593a47e978bf1c
John Gonsalves <thomasgonsalves12345@gmail.com>
2025-08-06 13:35:28 -0500

    working on the file

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,6 +1,17 @@
 import React, { useState } from "react";
+import { navLinks } from "./constants";
 const NavItems = () => {
-  return <div></div>;
+  return (
+    <ul className="nav-ul">
+      {navLinks.map(({ id, href, name }) => (
+        <li key={id} className="nav-li">
+          <a href={href} className="nav-li_a">
+            {href}
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 const Navbar = () => {
