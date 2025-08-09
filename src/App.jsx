c84186b0912f2a31aa06c5ea67134e687b3ddda4
John Gonsalves <thomasgonsalves12345@gmail.com>
2025-08-08 22:43:40 -0500

    working on the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./Navbar";
 
 const App = () => {
   return (
-    <main className="max-w-7xl mx-auto">
+    <main className="max-w-7xl mx-auto relative">
       <Navbar />
     </main>
   );
