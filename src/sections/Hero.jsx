9c3701f102fb4eb135fbe3a0015f98b8619d13e8
John Gonsalves <thomasgonsalves12345@gmail.com>
2025-08-21 13:02:07 -0500

    working on the 3d model

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,9 +3,18 @@ import { Canvas } from "@react-three/fiber";
 import React, { Suspense } from "react";
 import HackerRoom from "../components/HackerRoom";
 import CanvasLoader from "../components/CanvasLoader";
-import { Leva } from "leva";
+import { Leva, useControls } from "leva";
 
 const Hero = () => {
+  const x = useControls("HackerRoom", {
+    positionX: { value: 2.5, min: -10, max: 10 },
+    positionY: { value: 2.5, min: -10, max: 10 },
+    positionZ: { value: 2.5, min: -10, max: 10 },
+    rotationX: { value: 0, min: -10, max: 10 },
+    rotationY: { value: 0, min: -10, max: 10 },
+    rotationZ: { value: 0, min: -10, max: 10 },
+    scale: { value: 1, min: 0.1, max: 10 },
+  });
   return (
     <section className="min-h-screen w-full flex flex-col relative">
       <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
@@ -17,15 +26,15 @@ const Hero = () => {
         </p>
       </div>
       <div className="w-full h-full absolute inset-0">
+        <Leva />
         <Canvas className="w-full h-full">
           <Suspense fallback={<CanvasLoader />}>
             <PerspectiveCamera makeDefault position={[0, 0, 30]} />
             <HackerRoom
-              scale={0.05}
-              position={[0, 0, 0]}
-              rotation={[0, -Math.PI / 2, 0]}
+              position={[2, -8, 2]}
+              rotation={[0, -Math.PI, 0]}
+              scale={0.1}
             />
-            <Leva />
             <ambientLight intensity={1} />
             <directionalLight position={[10, 10, 5]} intensity={1} />
           </Suspense>
