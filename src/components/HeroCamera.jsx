18a244ed260e56534675170b999164d66bda7043
John Gonsalves <thomasgonsalves12345@gmail.com>
2025-09-10 18:44:47 -0500

    working on the about section

--- src/components/HeroCamera.jsx
+++ src/components/HeroCamera.jsx
@@ -2,19 +2,20 @@ import { useRef } from "react";
 import { useFrame } from "@react-three/fiber";
 import { easing } from "maath";
 
-const HeroCamera = ({ children, isMobile }) => {
-  const groupRef = useRef();
+const HeroCamera = ({ isMobile, children }) => {
+  const group = useRef();
   useFrame((state, delta) => {
     easing.damp3(state.camera.position[(0, 0, 20)], 0.25, delta);
     if (!isMobile) {
-      easing.damp3(state.camera.position, [0, 3, 25], 0.25, delta);
+      easing.dampE(
+        group.current.rotation,
+        [-state.pointer.y / 3, state.pointer.x / 5, 0],
+        0.25,
+        delta
+      );
     }
   });
-  return (
-    <group ref={groupRef} scale={isMobile ? 1 : 1.3}>
-      {children}
-    </group>
-  );
+  return <group ref={group}>{children}</group>;
 };
 
 export default HeroCamera;
--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,4 +1,13 @@
+import Globe from "react-globe.gl";
+import Button from "../components/Button";
+
 const About = () => {
+  const [hasCopied, setHasCopied] = useState(false);
+  const handleCopy = () => {
+    navigator.clipboard.writeText("thomasgonsalves12345@gmail.com");
+    setHasCopied(true);
+    setTimeout(() => setHasCopied(false), 2000);
+  };
   return (
     <section className="c-space my-28">
       <div className="grid xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5 h-full">
@@ -18,6 +27,86 @@ const About = () => {
                 aesthetics, I specialize in creating user-centric solutions that
                 seamlessly blend form and function.
               </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-span-1 xl:row-span-3">
+          <div className="grid-container">
+            <img
+              src="/assets.grid2.png"
+              alt="grid-2"
+              className="w-full sm:w-[276px] h-fit object-contain"
+            />
+            <div className="">
+              <p className="grid-headtext">What I Do</p>
+              <p className="grid-subtext">
+                i specialize in Javascript and React
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="col-span-1 xl:row-span-4">
+          <div className="grid-container">
+            <div className="rounded-3xl w-full sm:h-[236px] h-fit flex justify-center items-center">
+              <Globe
+                height={326}
+                width={326}
+                backgroundColor="rgba(0,0,0,0)"
+                backgroundImageOpacity={0.5}
+                showAtmosphere
+                showGraticules
+                globeImageUrl=""
+              />
+            </div>
+            <div className="">
+              <p className="grid-headtext">My Skills</p>
+              <p className="grid-subtext">
+                HTML, CSS, JavaScript, React, Node.js, Express.js, MongoDB, Git,
+                Figma, Adobe XD, Photoshop, Illustrator
+              </p>
+              <Button name="Contact Me" isBeam containerClass="w-full mt-10" />
+            </div>
+          </div>
+        </div>
+        <div className="xl:col-span-2 xl:row-span-3">
+          <div className="grid-container">
+            <img
+              src="/assets/grid3.png"
+              alt="grid-3"
+              className="w-full sm:h-[266px] h-fit object-contain"
+            />
+            <div className="">
+              <p className="grid-headtext">My Journey</p>
+              <p className="grid-subtext">
+                I embarked on my journey as a Product Designer and Brand, honing
+                my skills through hands-on experience and continuous learning.
+                My passion for design and technology has driven me to create
+                user- centric solutions that not only meet but exceed client
+                expectations. Over the years, I have had the privilege of
+                working with diverse clients, each project contributing to my
+                growth and expertise in the field.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="xl:col-span-1 xl:row-span-2">
+          <div className="grid-container">
+            <img
+              src="assets/grid4.png"
+              alt="grid-4"
+              className="w-full md:h-[126px] sm:h-[276px] h-fit object-cover sm:object-top"
+            />
+            <div className="space-y-2">
+              <p className="grid-subtext text-center">Contact me </p>
+              <div className="copy-container" onClick={handleCopy}>
+                <img
+                  src={hasCopied ? "assets/tick.svg" : "assets/copy.sbg"}
+                  alt="copy"
+                />
+                <p className="lg:text-2xl md;text-xl font-medium text-gray_gradiant text-white">
+                  thomasgonsalves12345@gmail.com
+                </p>
+              </div>
             </div>
           </div>
         </div>
